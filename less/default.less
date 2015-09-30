2e817ed3d5acc34b534bdbcff52bdfef52fac9a2
Romain Soufflet <romain.soufflet@nanocloud.com>
2015-09-30 15:59:18 +0200

    NC-141 Fix licence text
    Former-commit-id: ea7308b97617aeb628c63ab53eeddb1ed487f057

--- less/default.less
+++ less/default.less
@@ -1,5 +1,6 @@
 /*
- * Nanocloud community -- transform any application into SaaS solution
+ * Nanocloud Community, a comprehensive platform to turn any application
+ * into a cloud solution.
  *
  * Copyright (C) 2015 Nanocloud Software
  *
