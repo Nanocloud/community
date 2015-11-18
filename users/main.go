2e7701339663815f5434dd021bf8323df04e32ae
Antoine Duban <antoine.duban@nanocloud.com>
2015-11-18 16:54:52 +0100

    NC-250 Users, Owncloud and LDAP fully functionnal with RabbitMQ
    Former-commit-id: ff4c3daa6ce6d2df2e6862c07e2e01518fed112d

--- users/main.go
+++ users/main.go
@@ -27,16 +27,18 @@ type api struct{}
 var UserDb *bolt.DB
 
 type UserInfo struct {
-	Name     string
-	Email    string
-	Password string
+	Name      string
+	Email     string
+	Password  string
+	Activated string
+	Sam       string
 }
 
 type Message struct {
 	Method    string
 	Name      string
 	Email     string
-	Activated bool
+	Activated string
 	Sam       string
 	Password  string
 }
@@ -92,6 +94,86 @@ func GetList(users *[]UserInfo) error {
 	return nil
 }
 
+func GetUser(args PlugRequest, reply *PlugRequest) error {
+	initConf()
+	err := Configure()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer UserDb.Close()
+	var user UserInfo
+	err = json.Unmarshal([]byte(args.Body), &user)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	e := UserDb.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(conf.DatabaseName))
+		if bucket == nil {
+			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
+		}
+
+		userJson := bucket.Get([]byte(user.Email))
+		//json.Unmarshal(userJson, &user)
+		reply.Body = string(userJson)
+
+		return nil
+	})
+	if e != nil {
+		return e
+	}
+	return nil
+
+}
+
+type Registered struct {
+	IsRegistered string
+}
+
+func IsUserRegistered(args PlugRequest, reply *PlugRequest) error {
+	initConf()
+	err := Configure()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer UserDb.Close()
+	var user UserInfo
+	err = json.Unmarshal([]byte(args.Body), &user)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	str, err := json.Marshal(Registered{IsRegistered: "false"})
+	if err != nil {
+		log.Println(err)
+	}
+	reply.Body = string(str)
+	e := UserDb.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(conf.DatabaseName))
+		if bucket == nil {
+			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
+		}
+
+		cursor := bucket.Cursor()
+		for key, _ := cursor.First(); key != nil; key, _ = cursor.Next() {
+			if string(key) == user.Email {
+				str, err := json.Marshal(Registered{IsRegistered: "true"})
+				if err != nil {
+					log.Println(err)
+				}
+				reply.Body = string(str)
+				break
+			}
+		}
+
+		return nil
+	})
+	return e
+}
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
@@ -99,9 +181,40 @@ func failOnError(err error, msg string) {
 	}
 }
 
-var ch *amqp.Channel
-var q amqp.Queue
-var conn *amqp.Connection
+type NbUser struct {
+	Count int
+}
+
+func CountRegisteredUsers(args PlugRequest, reply *PlugRequest) error {
+	initConf()
+	err := Configure()
+	if err != nil {
+		log.Println(err)
+	}
+	var nb NbUser
+	nb.Count = 0
+	defer UserDb.Close()
+
+	e := UserDb.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(conf.DatabaseName))
+		if bucket == nil {
+			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
+		}
+
+		cursor := bucket.Cursor()
+		for key, _ := cursor.First(); key != nil; key, _ = cursor.Next() {
+			nb.Count++
+		}
+		str, err := json.Marshal(nb)
+		if err != nil {
+			log.Println(err)
+		}
+		reply.Body = string(str)
+		return nil
+	})
+
+	return e
+}
 
 func SendMsg(msg Message) {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
@@ -135,6 +248,8 @@ func SendMsg(msg Message) {
 	failOnError(err, "Failed to publish a message")
 
 	log.Printf(" [x] Sent order to plugin")
+	defer ch.Close()
+	defer conn.Close()
 
 }
 
@@ -150,13 +265,13 @@ func Add(args PlugRequest, reply *PlugRequest) error {
 	if err != nil {
 		log.Println(err)
 	}
-	SendMsg(Message{Method: "Add", Name: t.Name, Email: t.Email, Password: t.Password})
+	SendMsg(Message{Method: "Add", Name: t.Name, Email: t.Email, Password: t.Password, Activated: t.Activated})
 	UserDb.Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(conf.DatabaseName))
 		if bucket == nil {
 			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
 		}
-		bucket.Put([]byte(t.Name), []byte(args.Body))
+		bucket.Put([]byte(t.Email), []byte(args.Body))
 
 		return err
 	})
@@ -177,7 +292,7 @@ func ModifyPassword(args PlugRequest, reply *PlugRequest) error {
 	if err != nil {
 		log.Println(err)
 	}
-	SendMsg(Message{Method: "ChangePassword", Name: t.Name, Password: t.Password})
+	SendMsg(Message{Method: "ChangePassword", Name: t.Name, Password: t.Password, Email: t.Email})
 	UserDb.Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(conf.DatabaseName))
 		if bucket == nil {
@@ -189,7 +304,42 @@ func ModifyPassword(args PlugRequest, reply *PlugRequest) error {
 				json.Unmarshal(value, &rec)
 				rec.Password = t.Password
 				jsonUser, _ := json.Marshal(rec)
-				bucket.Put([]byte(rec.Name), jsonUser)
+				bucket.Put([]byte(rec.Email), jsonUser)
+				break
+			}
+		}
+
+		return err
+	})
+	return nil
+}
+
+func DisableAccount(args PlugRequest, reply *PlugRequest) error {
+	initConf()
+	err := Configure()
+	if err != nil {
+		log.Println(err)
+	}
+	defer UserDb.Close()
+	var t UserInfo
+	var rec UserInfo
+	err = json.Unmarshal([]byte(args.Body), &t)
+	if err != nil {
+		log.Println(err)
+	}
+	SendMsg(Message{Method: "DisableAccount", Email: t.Email, Name: t.Name})
+	UserDb.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(conf.DatabaseName))
+		if bucket == nil {
+			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
+		}
+		cursor := bucket.Cursor()
+		for key, value := cursor.First(); key != nil; key, value = cursor.Next() {
+			if string(key) == t.Email {
+				json.Unmarshal(value, &rec)
+				rec.Activated = t.Activated
+				jsonUser, _ := json.Marshal(rec)
+				bucket.Put([]byte(rec.Email), jsonUser)
 				break
 			}
 		}
@@ -211,13 +361,13 @@ func Delete(args PlugRequest, reply *PlugRequest) error {
 	if err != nil {
 		log.Println(err)
 	}
-	SendMsg(Message{Method: "Delete", Name: t.Name})
+	SendMsg(Message{Method: "Delete", Email: t.Email, Name: t.Name})
 	UserDb.Update(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte(conf.DatabaseName))
 		if bucket == nil {
 			return errors.New(fmt.Sprintf("Bucket '%s' doesn't exist", conf.DatabaseName))
 		}
-		bucket.Delete([]byte(t.Name))
+		bucket.Delete([]byte(t.Email))
 
 		return err
 	})
@@ -225,7 +375,7 @@ func Delete(args PlugRequest, reply *PlugRequest) error {
 	return nil
 }
 
-func ListCall(reply *PlugRequest) {
+func ListCall(args PlugRequest, reply *PlugRequest) error {
 	initConf()
 	err := Configure()
 	if err != nil {
@@ -236,24 +386,27 @@ func ListCall(reply *PlugRequest) {
 	GetList(&users)
 	rsp, err := json.Marshal(users)
 	reply.Body = string(rsp)
+	return nil
 
 }
 
 func (api) Receive(args PlugRequest, reply *PlugRequest) error {
+	handlers := make(map[string]func(PlugRequest, *PlugRequest) error, 8)
+	handlers["/users/add"] = Add
+	handlers["/users/list"] = ListCall
+	handlers["/users/delete"] = Delete
+	handlers["/users/modifypassword"] = ModifyPassword
+	handlers["/users/disableaccount"] = DisableAccount
+	handlers["/users/getuser"] = GetUser
+	handlers["/users/isuserregistered"] = IsUserRegistered
+	handlers["/users/countregisteredusers"] = CountRegisteredUsers
 
-	if strings.Index(args.Url, "/users/add") == 0 {
-		Add(args, reply)
+	for i, val := range handlers {
+		if strings.Index(args.Url, i) == 0 {
+			val(args, reply)
+			break
+		}
 	}
-	if strings.Index(args.Url, "/users/list") == 0 {
-		ListCall(reply)
-	}
-	if strings.Index(args.Url, "/users/delete") == 0 {
-		Delete(args, reply)
-	}
-	if strings.Index(args.Url, "/users/modifypassword") == 0 {
-		ModifyPassword(args, reply)
-	}
-
 	return nil
 }
 
@@ -266,7 +419,7 @@ func ListenToQueue() {
 	failOnError(err, "Failed to connect to RabbitMQ")
 	//defer conn.Close()
 
-	ch, err = conn.Channel()
+	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 
 	err = ch.ExchangeDeclare(
@@ -283,7 +436,7 @@ func ListenToQueue() {
 		"users", // name
 		false,   // durable
 		false,   // delete when usused
-		true,    // exclusive
+		false,   // exclusive
 		false,   // no-wait
 		nil,     // arguments
 	)
@@ -315,6 +468,8 @@ func ListenToQueue() {
 		}
 	}()
 	log.Println("Waiting for responses of fake/owncloud")
+	defer ch.Close()
+	defer conn.Close()
 	<-forever
 }
 
@@ -330,8 +485,6 @@ func (api) Check(args interface{}, reply *bool) error {
 }
 
 func (api) Unplug(args interface{}, reply *bool) error {
-	ch.Close()
-	conn.Close()
 	defer os.Exit(0)
 	*reply = true
 	return nil
