7836546d7251e866ea66ee28f865b4adf461a42b
Amin Khansari <amin.khansari@nanocloud.com>
2015-12-03 12:51:34 +0100

    Isolate the module
    Former-commit-id: a78583b301779fbc41e37351bd0e7589a7ea33c4

--- users/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  name             varchar(255),
-  email            varchar(36) PRIMARY KEY,
-  password         varchar(60),
-  activated        boolean,
-  sam              varchar(20)
-);
