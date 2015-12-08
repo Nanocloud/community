c0dc1ec0107bdfc140e0a6bb52664d1ef7805bd4
Amin Khansari <amin.khansari@nanocloud.com>
2015-12-08 11:06:00 +0100

    [NC-324] Add the users front
    Former-commit-id: f7e4d17e6e35071b96c92f8bf78d7ce4d4e08e49

---
+++ modules/users/front/website/ts/components/users/haptic.users.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../core.d.ts" />
+/// <amd-dependency path="angular-ui-router-extras" />
+import { overrideModuleRegisterer, registerCtrlFutureStates, getTemplateUrl } from "AmdTools";
+import { MainMenu } from "MainMenu";
+
+let componentName = "users";
+let app = angular.module("haptic." + componentName, ["ct.ui.router.extras.future"]);
+
+let states: angular.ui.IState[] = [{
+	name: "admin.users",
+	url: "/users",
+	controller: "UsersCtrl",
+	controllerAs: "usersCtrl",
+	templateUrl: getTemplateUrl(componentName, "users.html")
+}];
+
+MainMenu.add({
+	stateName: states[0].name,
+	title: "Users",
+	ico: "people"
+});
+
+app.config(["$controllerProvider", "$provide", "$futureStateProvider", function(
+	$controllerProvider: angular.IControllerProvider,
+	$provide: angular.auto.IProvideService,
+	$futureStateProvider: any) {
+
+	overrideModuleRegisterer(app, $controllerProvider, $provide);
+
+	registerCtrlFutureStates(componentName, $futureStateProvider, states);
+
+}]);
