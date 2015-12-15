976f7f92645dbc872bb1536f8511a8136b1706a5
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2015-12-15 12:01:41 +0100

    Delete certificate upload that does not work anyway

--- windows/floppy/windows-2012-standard-amd64/upload-cert.ps1
+++
@@ -1,4 +0,0 @@
-$url = "http://the.earth.li/~sgtatham/putty/latest/x86/pscp.exe"
-$output = "c:\windows\system32\pscp.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-echo n | C:\Windows\System32\pscp.exe -q -pw PASSWORD "C:\Users\Administrator\ad2012.cer" USER@HOST:PATH
