75772ce5b9c7c4a22e643840d3a5d746f9b67d64
William Riancho <wr.wllm@gmail.com>
2015-12-29 15:17:08 +0100

    [NC-376] RabbitMQ RPC instead of pie
    Former-commit-id: 6f566f08504ab37983e97cdb769f839be7f5eafb

--- modules/history/main.go
+++ modules/history/main.go
@@ -26,31 +26,16 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
-	log "github.com/Sirupsen/logrus"
+	"github.com/Nanocloud/nano"
 	_ "github.com/lib/pq"
-	"net/http"
-	"net/rpc/jsonrpc"
-	"net/url"
 	"os"
-	"regexp"
-
-	"github.com/natefinch/pie"
+	"time"
 )
 
-var (
-	name = "history"
-	srv  pie.Server
-)
-
-type HistoryConfig struct {
-	ConnectionString string
-	DatabaseName     string
-}
-
-// Plugin Structure
-type api struct{}
-
+var module nano.Module
 var db *sql.DB
+
+type hash map[string]interface{}
 
 // Log entries are stored in this structure
 type HistoryInfo struct {
@@ -60,16 +45,23 @@ type HistoryInfo struct {
 	EndDate      string
 }
 
-// Structure used for exchanges with the core, faking a request/responsewriter
-type PlugRequest struct {
-	Body     string
-	Header   http.Header
-	Form     url.Values
-	PostForm url.Values
-	Url      string
-	Method   string
-	HeadVals map[string]string
-	Status   int
+func dbConnect() {
+	databaseURI := os.Getenv("DATABASE_URI")
+	if len(databaseURI) == 0 {
+		databaseURI = "postgres://localhost/nanocloud?sslmode=disable"
+	}
+
+	var err error
+
+	for try := 0; try < 10; try++ {
+		db, err = sql.Open("postgres", databaseURI)
+		if err == nil {
+			return
+		}
+		time.Sleep(time.Second * 5)
+	}
+
+	module.Log.Fatalf("Cannot connect to Postgres Database: %s", err)
 }
 
 // Connects to the postgres databse
@@ -84,7 +76,7 @@ func setupDb() error {
 	defer rows.Close()
 
 	if rows.Next() {
-		log.Info("[History] histories table already set up\n")
+		module.Log.Info("Histories table already set up")
 		return nil
 	}
 
@@ -96,7 +88,7 @@ func setupDb() error {
 			enddate		varchar(36)
 		);`)
 	if err != nil {
-		log.Errorf("[History] Unable to create histories table: %s\n", err)
+		module.Log.Errorf("Unable to create histories table: %s", err)
 		return err
 	}
 
@@ -105,14 +97,15 @@ func setupDb() error {
 }
 
 // Get a list of all the log entries of the database
-func GetList(histories *[]HistoryInfo) error {
+func ListCall(req nano.Request) (*nano.Response, error) {
+	var histories []HistoryInfo
 	rows, err := db.Query(
 		`SELECT userid, connectionid,
 		startdate, enddate
 		FROM histories`,
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer rows.Close()
@@ -125,28 +118,25 @@ func GetList(histories *[]HistoryInfo) error {
 			&history.StartDate,
 			&history.EndDate,
 		)
-		*histories = append(*histories, history)
+		histories = append(histories, history)
 	}
 
 	err = rows.Err()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
-
+	return nano.JSONResponse(200, histories), nil
 }
 
 // Add a new log entry to the database
-func Add(args PlugRequest, reply *PlugRequest) error {
-
+func AddCall(req nano.Request) (*nano.Response, error) {
 	var t HistoryInfo
-	err := json.Unmarshal([]byte(args.Body), &t)
+	err := json.Unmarshal([]byte(req.Body), &t)
 	if err != nil {
-		reply.Status = 400
-		log.Error("Json Arguments are not valid: ", err)
-		return err
+		return nil, err
 	}
+
 	rows, err := db.Query(
 		`INSERT INTO histories
 		(userid, connectionid, startdate, enddate)
@@ -157,116 +147,24 @@ func Add(args PlugRequest, reply *PlugRequest) error {
 		case "pq: duplicate key value violates unique constraint \"history_pkey\"":
 			err = errors.New("history connection id exists already")
 		}
-		return err
+		return nil, err
 	}
 
 	rows.Close()
-	return err
 
-}
-
-// Connects to the DB, adds entry ,sets the status and body of the response and closes the DB
-func AddCall(args PlugRequest, reply *PlugRequest, id string) {
-	reply.HeadVals = make(map[string]string, 1)
-	reply.HeadVals["Content-Type"] = "application/json;charset=UTF-8"
-	initConf()
-	err := Add(args, reply)
-	if err != nil {
-		if reply.Status != 400 {
-			reply.Status = 500
-		}
-		log.Error("Failed to add an entry: ", err)
-		return
-	}
-	reply.Status = 202
-}
-
-// Connects to the DB, list entries ,sets the status and body of the response and closes the DB
-func ListCall(args PlugRequest, reply *PlugRequest, id string) {
-	reply.HeadVals = make(map[string]string, 1)
-	reply.HeadVals["Content-Type"] = "application/json;charset=UTF-8"
-	initConf()
-	var histories []HistoryInfo
-	GetList(&histories)
-	rsp, err := json.Marshal(histories)
-	if err != nil {
-		reply.Status = 500
-		log.Error("Failed to Marshal histories: ", err)
-		return
-	}
-	reply.Body = string(rsp)
-	reply.Status = 200
-
-}
-
-// slice of available handler functions
-var tab = []struct {
-	Url    string
-	Method string
-	f      func(PlugRequest, *PlugRequest, string)
-}{
-	{`^\/api\/history\/{0,1}$`, "GET", ListCall},
-	{`^\/api\/history\/{0,1}$`, "POST", AddCall},
-}
-
-// Will receive all http requests starting by /api/history from the core and chose the correct handler function
-func (api) Receive(args PlugRequest, reply *PlugRequest) error {
-	for _, val := range tab {
-		re := regexp.MustCompile(val.Url)
-		match := re.MatchString(args.Url)
-		if val.Method == args.Method && match {
-			if len(re.FindStringSubmatch(args.Url)) == 2 {
-				val.f(args, reply, re.FindStringSubmatch(args.Url)[1])
-				return nil
-			} else {
-				val.f(args, reply, "")
-				return nil
-			}
-		}
-	}
-	return nil
-}
-
-// Plug the plugin to the core
-func (api) Plug(args interface{}, reply *bool) error {
-	*reply = true
-	return nil
-}
-
-// Will contain various verifications for the plugin. If the core can call the function and receives "true" in the reply, it means the plugin is functionning correctly
-func (api) Check(args interface{}, reply *bool) error {
-	*reply = true
-	return nil
-}
-
-// Unplug the plugin from the core
-func (api) Unplug(args interface{}, reply *bool) error {
-	defer os.Exit(0)
-	*reply = true
-	return nil
+	return nano.JSONResponse(200, hash{
+		"success": true,
+	}), nil
 }
 
 func main() {
+	module = nano.RegisterModule("history")
 
-	log.SetOutput(os.Stderr)
-	log.SetLevel(log.DebugLevel)
+	dbConnect()
+	setupDb()
 
-	initConf()
-	srv = pie.NewProvider()
-	if err := srv.RegisterName(name, api{}); err != nil {
-		log.Fatal("Failed to register:", name, err)
-	}
+	module.Get("/history", ListCall)
+	module.Post("/history", AddCall)
 
-	var err error
-	db, err = sql.Open("postgres", conf.DatabaseUri)
-	if err != nil {
-		log.Fatalf("Cannot connect to Postgres Database: %s", err)
-	}
-
-	err = setupDb()
-	if err != nil {
-		log.Fatalf("[History] unable to setup histories table: %s", err)
-	}
-
-	srv.ServeCodec(jsonrpc.NewServerCodec)
+	module.Listen()
 }
