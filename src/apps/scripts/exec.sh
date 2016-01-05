1008e7e03c423c598907bceb96f1ffc94d7ccbbe
Antoine Duban <blodnasir@gmail.com>
2016-01-05 14:06:53 +0100

    NC-243 exec.sh use correct environment variable

--- src/apps/scripts/exec.sh
+++ src/apps/scripts/exec.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Load configuration file
-source ../src/apps/scripts/configuration.sh
-
 if [ ${#} -lt 1 ]; then
-    echo "Not enough arguments"
-      exit 1
-    fi
+	echo "Not enough arguments"
+	exit 1
+fi
 
-    COMMAND=${1}
-    SSH=$(which ssh)
+COMMAND=${1}
+SSH=$(which ssh)
 
-    sshpass -p "${PASSWORD}" "${SSH}" -o StrictHostKeyChecking=no -p "${PORT}" "${USER}@${SERVER}" "${COMMAND}"
+sshpass -p "${PASSWORD}" "${SSH}" -o StrictHostKeyChecking=no -p "${SSH_PORT}" "${USER}@${SERVER}" "${COMMAND}"
