4b95644feb5ff06b74a7ee02a50c6c593f30962b
William Riancho <wr.wllm@gmail.com>
2016-01-05 14:12:31 +0100

    file copy fixed
    Former-commit-id: 73783ea800295d54f751db9815770c40d2ca7591

--- nanocloud/scripts/copy.sh
+++ nanocloud/scripts/copy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Load configuration file
-source configuration.sh
-
 if [ ${#} -lt 1 ]; then
-    echo "Not enough arguments"
-      exit 1
-    fi
+  echo "Not enough arguments"
+  exit 1
+fi
 
-    FILENAME=${1}
-    SCP=$(which scp)
+FILENAME=${1}
+SCP=$(which scp)
 
-    sshpass -p "${PASSWORD}" "${SCP}" -o StrictHostKeyChecking=no -P "${PORT}" "${FILENAME}" "${USER}@${SERVER}:~/Desktop/"
-    if [ $? -eq 0 ]; then
-        rm "${FILENAME}"
-      fi
+sshpass -p "${WIN_PASSWORD}" "${SCP}" -o StrictHostKeyChecking=no -P "${WIN_PORT}" "${FILENAME}" "${WIN_USER}@${WIN_SERVER}:~/Desktop/"
+if [ $? -eq 0 ]; then
+  rm "${FILENAME}"
+fi
