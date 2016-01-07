281ef24fd34874d83fd5e8198e5145c7ddf4c4ba
Antoine Duban <blodnasir@gmail.com>
2016-01-07 18:00:38 +0100

    deleted  unnecessary file
    Former-commit-id: 47f8f11f07b1bd412d157c01a34a2731795c4e25

--- src/ldap/script/getcert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p /opt/conf
-sshpass -p ${PASSWORD} scp -P ${SSH_PORT} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${USER}@${SERVER}:/cygdrive/c/users/administrator/ad2012.cer /opt/conf
