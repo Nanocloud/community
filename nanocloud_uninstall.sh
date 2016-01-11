48c9cc714ec77c38b96f9aaf8e780880b950fbdf
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-01-11 16:09:21 +0100

    remove docker containers and docker images
    Both containers and images need to be removed

--- nanocloud_uninstall.sh
+++ nanocloud_uninstall.sh
@@ -20,7 +20,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-NANOCLOUD_DIR=${NANOCLOUD_DIR:-"/var/lib/nanocloud"}
+
+SCRIPT_FULL_PATH=$(readlink -e "${0}")
+CURRENT_DIR=$(dirname "${SCRIPT_FULL_PATH}")
 DATE_FMT="+%Y/%m/%d %H:%M:%S"
 
 NANOCLOUD_DIR=${NANOCLOUD_DIR:-"${CURRENT_DIR}/installation_dir"}
@@ -34,5 +36,33 @@ if [ -z "$(which docker-compose)" ]; then
   exit 2
 fi
 
+rm -rf ${CURRENT_DIR}/dockerfiles/build_output
+docker-compose -f ${CURRENT_DIR}/dockerfiles/docker-compose.yml rm -f > /dev/null 2>&1
+docker rmi -f guacamole-client \
+       dockerfiles_guacamole-server \
+       dockerfiles_nanocloud-api \
+       dockerfiles_proxy \
+       dockerfiles_ambassador \
+       dockerfiles_rabbitmq \
+       dockerfiles_postgres \
+       dockerfiles_apps-module \
+       dockerfiles_history-module \
+       dockerfiles_iaas-module \
+       dockerfiles_apiiaas-module \
+       dockerfiles_ldap-module \
+       dockerfiles_users-module > /dev/null 2>&1
+docker rmi -f nanocloud/guacamole-client \
+       nanocloud/guacamole-server \
+       nanocloud/nanocloud-api \
+       nanocloud/proxy \
+       nanocloud/ambassador \
+       nanocloud/rabbitmq \
+       nanocloud/postgres \
+       nanocloud/apps-module \
+       nanocloud/history-module \
+       nanocloud/iaas-module \
+       nanocloud/apiiaas-module \
+       nanocloud/ldap-module \
+       nanocloud/users-module > /dev/null 2>&1
 
 echo "$(date "${DATE_FMT}") Removing installed files"
