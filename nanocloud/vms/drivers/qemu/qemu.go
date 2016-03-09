d1fe95fc236f0c1ee631c7089c85054e7618411f
Antoine Duban <blodnasir@gmail.com>
2016-03-09 11:26:41 +0100

    deleted unnecessary url for windows iso

--- nanocloud/vms/drivers/qemu/qemu.go
+++ nanocloud/vms/drivers/qemu/qemu.go
@@ -2,10 +2,6 @@ package qemu
 
 import "github.com/Nanocloud/community/nanocloud/vms"
 
-const (
-	windowsIsoUri = "http://care.dlservice.microsoft.com/dl/download/6/2/A/62A76ABB-9990-4EFC-A4FE-C7D698DAEB96/9600.17050.WINBLUE_REFRESH.140317-1640_X64FRE_SERVER_EVAL_EN-US-IR3_SSS_X64FREE_EN-US_DV9.ISO"
-)
-
 func init() {
 	vms.Register("qemu", &driver{})
 }
