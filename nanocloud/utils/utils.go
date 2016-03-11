55de1b8305169fbeb39fc3e2938a1e71c7d079c5
Antoine Duban <blodnasir@gmail.com>
2016-03-11 13:48:47 +0100

    Reverting changed to echo Context interface

--- nanocloud/utils/utils.go
+++ nanocloud/utils/utils.go
@@ -93,7 +93,7 @@ func CopyFile(source string, dest string) (err error) {
 	return
 }
 
-func ParseJSONBody(c echo.Context, dst interface{}) error {
+func ParseJSONBody(c *echo.Context, dst interface{}) error {
 	body, err := ioutil.ReadAll(c.Request().Body)
 	if err == nil {
 		err = json.Unmarshal(body, dst)
