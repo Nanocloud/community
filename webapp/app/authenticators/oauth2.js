e24db0b34b14fbc52e2d5d9d5f38130b1b52b437
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-12 23:06:34 +0100

    Do not extend OAuth2PasswordGrant anymore

--- webapp/app/authenticators/oauth2.js
+++ webapp/app/authenticators/oauth2.js
@@ -4,33 +4,5 @@ import Ember from 'ember';
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: 'oauth/token',
 
-  clientId: '9405fb6b0e59d2997e3c777a22d8f0e617a9f5b36b6565c7579e5be6deb8f7ae:9050d67c2be0943f2c63507052ddedb3ae34a30e39bbbbdab241c93f8b5cf341',
-
-  authenticate: function(username, password) {
-
-    return new Ember.RSVP.Promise((resolve, reject) => {
-
-      const serverTokenEndpoint = this.get('serverTokenEndpoint');
-      const options = {
-        url: serverTokenEndpoint,
-        data: JSON.stringify({
-          grant_type: 'password',
-          username: username,
-          password: password
-        }),
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json',
-        headers: {
-          Authorization: 'Basic ' + window.btoa(this.get('clientId'))
-        }
-      };
-
-      Ember.$.ajax(options).then(function (response) {
-          resolve(response);
-      }, function (xhr) {
-        reject(xhr.responseJSON || xhr.responseText);
-      });
-    });
-  }
+  clientId: '9405fb6b0e59d2997e3c777a22d8f0e617a9f5b36b6565c7579e5be6deb8f7ae'
 });
