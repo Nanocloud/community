31eb686c70e3889590cc7525256e1410e9664949
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 02:22:52 +0100

    Force clear login and password field on transition

--- webapp/app/controllers/login.js
+++ webapp/app/controllers/login.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  identification: null,
+  password: null,
 
   actions: {
     authenticate: function() {
