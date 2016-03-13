505816ab5ef91b156cd3d1fc93f279d0505af52d
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 15:53:44 +0100

    Remove unused variable

--- webapp/app/authenticators/oauth2.js
+++ webapp/app/authenticators/oauth2.js
@@ -1,5 +1,4 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
-import Ember from 'ember';
 
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: 'oauth/token',
