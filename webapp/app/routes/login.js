609599e60f5094960422e1296b85d3a598565ccc
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 02:28:46 +0100

    Redirect to login is not loggedin, redirect to services if index

--- webapp/app/routes/login.js
+++ webapp/app/routes/login.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
+
+  beforeModel() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('/');
+    }
+  }
 });
