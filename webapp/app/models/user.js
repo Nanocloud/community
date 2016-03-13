e5ee4dcc7e42d01a27b064bf4cb0681deb6ead88
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 18:09:56 +0100

    Display user full name in one column instead of its first name and last name

--- webapp/app/models/user.js
+++ webapp/app/models/user.js
@@ -7,4 +7,8 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   password: DS.attr('string'),
+
+  fullName: function() {
+    return `${this.get('firstName')}  ${this.get('lastName')}`;
+  }.property('firstName', 'lastName'),
 });
