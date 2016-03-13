21d9c543d68488e5de238795ea52e5f392d24c42
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 12:09:09 +0100

    Display Nanocloud logo in sidebar
    Sidebar display is handled via javascript
    Top bar remains fix on top
    Get rid of all z-index in favor of one master z-index to make sure navbar stick on top

--- webapp/app/controllers/protected.js
+++ webapp/app/controllers/protected.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   showSidebar: false,
+
+  evaluteWindow: function() {
+
+    let testWindow = function() {
+      if (Ember.$(window).width() > 991) {
+        this.set('showSidebar', true);
+      } else {
+        this.set('showSidebar', false);
+      }
+    }.bind(this)
+
+    Ember.$(window).resize(testWindow);
+
+    testWindow();
+  }.on("init"),
+
   actions: {
     toggleSidebar() {
       this.toggleProperty('showSidebar');
