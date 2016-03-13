8abc029b2496c2ad3e92ee5ef46ba9b5815a3e04
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-13 15:32:09 +0100

    Load user and access_token in global session object
    this.get('session.access_token') and this.get('session.user') respectivelly refer to currently loggedin user access_token and object data

--- webapp/app/routes/protected.js
+++ webapp/app/routes/protected.js
@@ -9,10 +9,37 @@ export default Ember.Route.extend({
     }
   },
 
-  actions: {
-    invalidateSession: function() {
+  model: function() {
+    this.set('session.access_token', this.get('session.data.authenticated.access_token'));
+
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      const options = {
+        url: 'api/me',
+        headers: {
+          Authorization: 'Bearer ' + this.get('session.access_token')
+        },
+        type:        'GET',
+        dataType:    'json'
+      };
+
+      Ember.$.ajax(options).then((user) => {
+        this.set('session.user', user.data);
+        resolve(user);
+      }, (issue) => {
+        this._invalidateSession();
+        reject(issue);
+      });
+    };
+  },
+
+  _invalidateSession: function() {
       this.get('session').invalidate();
       this.transitionTo('login');
+  },
+
+  actions: {
+    invalidateSession: function() {
+      this._invalidateSession();
     }
   }
 });
