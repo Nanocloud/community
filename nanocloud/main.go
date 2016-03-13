0260eac014153b8b8cbcf5b2d5273bf4c477ebb0
Eric Cao <eric.cao@nanocloud.com>
2016-03-13 21:53:53 +0100

    adding applications to display and run applications.

--- nanocloud/main.go
+++ nanocloud/main.go
@@ -108,12 +108,12 @@ func main() {
 	/**
 	 * APPS
 	 */
-	e.Get("/api/apps", m.OAuth2(m.Admin(apps.ListApplications)))
-	e.Delete("/api/apps/:app_id", m.OAuth2(m.Admin(apps.UnpublishApplication)))
-	e.Get("/api/apps/me", m.OAuth2(apps.ListUserApps))
-	e.Post("/api/apps", m.OAuth2(m.Admin(apps.PublishApplication)))
-	e.Get("/api/apps/connections", m.OAuth2(apps.GetConnections))
-	e.Patch("/api/apps/:app_id", m.OAuth2(m.Admin(apps.ChangeAppName)))
+	e.Get("/api/applications", m.OAuth2(m.Admin(apps.ListApplications)))
+	e.Delete("/api/applications/:app_id", m.OAuth2(m.Admin(apps.UnpublishApplication)))
+	e.Get("/api/applications/me", m.OAuth2(apps.ListUserApps))
+	e.Post("/api/applications", m.OAuth2(m.Admin(apps.PublishApplication)))
+	e.Get("/api/applications/connections", m.OAuth2(apps.GetConnections))
+	e.Patch("/api/applications/:app_id", m.OAuth2(m.Admin(apps.ChangeAppName)))
 
 	go appsModel.CheckPublishedApps()
 
