b3e63bc616a426c425e49ca058fcd847cb7598fb
Eric Cao <eric.cao@nanocloud.com>
2016-03-13 22:07:38 +0100

    fix missing column in user.js

--- webapp/app/routes/protected/users/user.js
+++ webapp/app/routes/protected/users/user.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   setupController: function(controller, model) {
-    controller.set('model', model)
+    controller.set('model', model);
     controller.set('editingPassword', false);
     controller.set('editingPasssword', "");
     controller.set('errorMessage', "");
