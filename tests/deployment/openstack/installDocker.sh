c9e5ffacb51347d2c9cb95eb193c70d8d0042371
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-03-22 19:41:12 +0100

    Prevent the whole script from exiting if something wrong happened during the installation

--- tests/deployment/openstack/installDocker.sh
+++ tests/deployment/openstack/installDocker.sh
@@ -20,8 +20,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
-
 sudo apt-get update
 sudo apt-get install -y curl
 
