a2be1af797daa9ac01bb3de9c4f713b4996bb864
William Riancho <william.riancho@nanocloud.com>
2016-03-30 10:26:27 +0200

    indentation fixed

--- webapp/app/login/controller.js
+++ webapp/app/login/controller.js
@@ -7,12 +7,17 @@ export default Ember.Controller.extend({
     authenticate: function() {
       let { identification, password } = this.getProperties('identification', 'password');
 
-      this.get('session').authenticate('authenticator:oauth2', identification, password)
-        .then(() => {
-          this.transitionToRoute('/');
-        })
-        .catch((reason) => {
-          this.set('errorMessage', reason.error || reason);
+      this.get('session')
+      .authenticate(
+        'authenticator:oauth2',
+        identification,
+        password
+      )
+      .then(() => {
+        this.transitionToRoute('protected.index');
+      })
+      .catch((reason) => {
+        this.set('errorMessage', reason.error || reason);
       });
     }
   }
