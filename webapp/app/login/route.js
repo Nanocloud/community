22895d82a1a0a22e9f9b8acf28e0e9a19b018c61
William Riancho <william.riancho@nanocloud.com>
2016-03-31 17:18:44 +0200

    reset login controller

--- webapp/app/login/route.js
+++ webapp/app/login/route.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller) {
+    controller.reset();
+    this._super(...arguments);
+  },
+
   redirect() {
     if (this.get('session.isAuthenticated')) {
       this.transitionTo('protected.index');
