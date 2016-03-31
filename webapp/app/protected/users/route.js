c94fff92696f7829e82b5303d814b4a9deb6fe58
William Riancho <william.riancho@nanocloud.com>
2016-03-31 14:35:38 +0200

    force model reload

--- webapp/app/protected/users/route.js
+++ webapp/app/protected/users/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.findAll('user');
+  model() {
+    return this.store.findAll('user', { reload: true });
   }
 });
