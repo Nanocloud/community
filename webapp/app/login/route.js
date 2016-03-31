254c4ff9daaeb43418a7316d71ca158ef543144f
William Riancho <william.riancho@nanocloud.com>
2016-03-31 16:01:11 +0200

    redirect to named route instead of path

--- webapp/app/login/route.js
+++ webapp/app/login/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   redirect() {
     if (this.get('session.isAuthenticated')) {
-      this.transitionTo('/');
+      this.transitionTo('protected.index');
     }
   }
 });
