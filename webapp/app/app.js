103fabf96a73ada995a41e00ba0b1f88ef13bc46
William Riancho <william.riancho@nanocloud.com>
2016-03-31 14:55:39 +0200

    log app info at boot

--- webapp/app/app.js
+++ webapp/app/app.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import config from 'nanocloud/config/environment';
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
+
+Ember.libraries.register(config.APP.name, config.APP.version);
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
