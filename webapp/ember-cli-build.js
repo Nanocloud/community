eb0458056fa831c7735e29fb200713594ac98967
Eric Cao <eric.cao@nanocloud.com>
2016-04-01 11:43:46 +0200

    add font-awesome for loading icons

--- webapp/ember-cli-build.js
+++ webapp/ember-cli-build.js
@@ -5,6 +5,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    emberCliFontAwesome: {
+      useScss: true, // for ember-cli-sass 
+      useLess: true  // for ember-cli-less 
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
