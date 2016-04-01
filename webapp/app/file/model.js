8257f7a0588de7a25ecc91f15e242dbd4223cba8
Eric Cao <eric.cao@nanocloud.com>
2016-04-01 17:45:06 +0200

    fix jshint error

--- webapp/app/file/model.js
+++ webapp/app/file/model.js
@@ -3,8 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   type: DS.attr('string'),
   icon: function() {
-    if (this.get('type') == 'directory')
+    if (this.get('type') === 'directory') {
       return ('folder');
+    }
     return ('description');
   }.property(),
   isSelected: false,
