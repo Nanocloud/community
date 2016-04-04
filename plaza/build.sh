2541d9fdccfd246154c7dd2c10b91cd64736fd2d
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-04-04 20:19:33 +0200

    Allow to build plaza from a Docker container
    Avoid dependencies issues while building plaza by provising a way to build the executable from a Docker container.
    
    Usage: ./build.sh docker

--- plaza/build.sh
+++ plaza/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-export GOOS=windows
-export GOARCH=amd64
+COMMAND=${1}
 
-go build
+if [ "${COMMAND}" = "docker" ]; then
+    docker build -t nanocloud/plaza .
+    docker run -d --name plaza nanocloud/plaza
+    docker cp plaza:/go/src/github.com/Nanocloud/community/plaza/plaza.exe .
+    docker kill plaza
+    docker rm plaza
+    docker rmi nanocloud/plaza
+else
+    export GOOS=windows
+    export GOARCH=amd64
+
+    go build
+fi
