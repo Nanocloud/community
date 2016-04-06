e6c53551a713f4bee8b6a5c2b56aed55ecfc3843
Eric Cao <eric.cao@nanocloud.com>
2016-04-06 23:27:19 +0200

    use background rgba on drag-and-drop so the text desn't turn gray

--- webapp/app/components/topbar-item/component.js
+++ webapp/app/components/topbar-item/component.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   actions: {
-  
     clickAction() {
       this.sendAction("click");
     }
--- webapp/app/components/vdi-drag-n-drop/component.js
+++ webapp/app/components/vdi-drag-n-drop/component.js
@@ -5,22 +5,16 @@ export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   flow: null,
   state: null,
-  defaultState: " - ",
-
-  initializeState:function() {
-    this.set('state', this.get('defaultState'));
-  }.on('init'),
-
-  updateUploadStatus: function () {
-    this.set('getUploadState', this.get('state') || this.get('defaultState'));
-  }.observes('state'),
-
+  progress: null,
+  
   showElement() {
-    Ember.$('.drag-n-drop-area').css("opacity", "0.6");
+    Ember.$('.drag-n-drop-area').css("background-color", "rgba(0, 144, 255, 0.6)");
+    Ember.$('.drag-n-drop-area .text').css("opacity", "1");
   },
 
   hideElement() {
-    Ember.$('.drag-n-drop-area').css("opacity", "0");
+    Ember.$('.drag-n-drop-area').css("background-color", "rgba(0, 144, 255, 0)");
+    Ember.$('.drag-n-drop-area .text').css("opacity", "0");
   },
 
   dragEnter() {
@@ -38,6 +32,7 @@ export default Ember.Component.extend({
   didInsertElement() {
 
     this.flow = new window.Flow({
+      chunkSize: 100000000,
       target: '/upload',
       headers: { Authorization : "Bearer " + this.get('session.access_token') },
       singleFile: true
@@ -50,19 +45,13 @@ export default Ember.Component.extend({
     });
 
     this.flow.on('complete', () => {
-      if (!this.aborted) {
+      if (this.get('state') !== 'Aborted') {
+        this.toast.success("Your file has been uploaded successfully!");
         this.set('state', "Completed");
-      }
-      else {
-        this.set('state', "Aborted");
       }
       setTimeout(() => {
         this.set('state', null);
       }, 3000);
-    });
-
-    this.flow.on('uploadStart', () => {
-      this.set('state', 0);
     });
 
     this.flow.on('error', () => {
@@ -70,19 +59,31 @@ export default Ember.Component.extend({
     });
 
     this.flow.on('fileProgress', (flow) => {
-      this.set('state', Math.floor(flow.progress() * 100));
-      if (this.get('state') === 99) {
+
+      this.set('time', flow.timeRemaining());
+      this.set('sizeUploaded', flow.sizeUploaded());
+      this.set('progress', Math.floor(flow.progress() * 100));
+      var progress = this.get('progress');
+      if (progress >= 90 && progress <= 100) {
         this.set('state', 'Reassembling');
+        this.set('progress', null);
+      }
+      else {
+        this.set('state', null);
       }
     });
   },
 
-  actions: {
-
-    cancelDownload() {
-      this.set('state', "Aborted");
-      this.flow.cancel();
-    }
+  stopUpload() {
+    this.toast.info("Your upload has been aborted successfully!");
+    this.set('state', "Aborted");
+    this.set('progress', null);
+    this.flow.cancel();
   },
 
+  actions: {
+    cancelUpload() {
+      this.stopUpload();
+    }
+  },
 });
