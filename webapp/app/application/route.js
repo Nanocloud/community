758aa491d9a9bc800e116e04a3b0c6ab39111edf
William Riancho <william.riancho@nanocloud.com>
2016-04-07 14:24:32 +0200

    unload store on logout

--- webapp/app/application/route.js
+++ webapp/app/application/route.js
@@ -3,6 +3,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   sessionInvalidated() {
+    this.store.unloadAll();
     this.toast.info('Your session has expired.');
     this.transitionTo('login');
   },
