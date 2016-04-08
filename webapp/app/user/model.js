5f8ecf6f5993fcf03cc63fca39f6c874c9abe0fe
Olivier Berthonneau <olivier.berthonneau@nanocloud.com>
2016-04-08 18:40:20 +0200

    Set firstname and lastname to be comprise between 2 and 254 characters

--- webapp/app/user/model.js
+++ webapp/app/user/model.js
@@ -2,8 +2,20 @@ import DS from 'ember-data';
 import {validator, buildValidations} from 'ember-cp-validations';
 
 const Validations = buildValidations({
-  firstName: validator('presence', true),
-  lastName: validator('presence', true),
+  firstName: [
+    validator('presence', true),
+    validator('length', {
+      min: 2,
+      max: 255
+    })
+  ],
+  lastName: [
+    validator('presence', true),
+    validator('length', {
+      min: 2,
+      max: 255
+    })
+  ],
   password: [
     validator('presence', true),
     validator('length', {
