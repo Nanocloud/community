e3f2137bdfc11509217d2713810ac2b6f3670cee
Romain Soufflet <romain.soufflet@nanocloud.com>
2016-04-15 10:37:22 +0200

    Change LDAPS port in openstack deployments

--- tests/deployment/openstack/installCommunity.sh
+++ tests/deployment/openstack/installCommunity.sh
@@ -6,5 +6,6 @@ git clone https://github.com/Nanocloud/community.git
 sed -i -e "s/=iaas-module/=$WIN_SERVER/" community/modules/docker-compose.yml
 sed -i -e "s/IAAS=qemu/IAAS=manual/" community/modules/docker-compose.yml
 sed -i -e "s/_PORT=1119/_PORT=22/" community/modules/docker-compose.yml
+sed -i -e "s/6360/=636/" community/modules/docker-compose.yml
 
 docker-compose -f community/modules/docker-compose-build.yml up -d
