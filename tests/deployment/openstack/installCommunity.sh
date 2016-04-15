02e86841e5e51c16fd108984ec60204d788f53df
Romain Soufflet <romain.soufflet@nanocloud.com>
2016-04-15 11:40:35 +0200

    Fix LDAPS connection string IP replacement.

--- tests/deployment/openstack/installCommunity.sh
+++ tests/deployment/openstack/installCommunity.sh
@@ -6,7 +6,7 @@ git clone https://github.com/Nanocloud/community.git
 sed -i -e "s/=iaas-module/=$WIN_SERVER/" community/modules/docker-compose.yml
 sed -i -e "s/IAAS=qemu/IAAS=manual/" community/modules/docker-compose.yml
 sed -i -e "s/_PORT=1119/_PORT=22/" community/modules/docker-compose.yml
-sed -i -e "s/6360/=636/" community/modules/docker-compose.yml
+sed -i -e "s/iaas-module:6360/=$WIN_SERVER:636/" community/modules/docker-compose.yml
 
 docker-compose -f community/modules/docker-compose-build.yml build
 docker-compose -f community/modules/docker-compose-build.yml up -d
