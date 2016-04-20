853ac8bc37dff0cf6d5cf2174813ca25c2ac482a
Romain Soufflet <romain.soufflet@nanocloud.com>
2016-04-20 10:12:46 +0200

    Add a loop to retry installation of Community

--- tests/deployment/openstack/installCommunity.sh
+++ tests/deployment/openstack/installCommunity.sh
@@ -8,5 +8,16 @@ sed -i -e "s/IAAS=qemu/IAAS=manual/" community/modules/docker-compose.yml
 sed -i -e "s/_PORT=1119/_PORT=22/" community/modules/docker-compose.yml
 sed -i -e "s/iaas-module:6360/=$WIN_SERVER:636/" community/modules/docker-compose.yml
 
-docker-compose -f community/modules/docker-compose-build.yml build
-docker-compose -f community/modules/docker-compose-build.yml up -d
+n=0
+until [ $n -ge 5 ]; do
+	docker-compose -f community/modules/docker-compose-build.yml build
+	docker-compose -f community/modules/docker-compose-build.yml up -d
+	NANOCLOUD_STATUS=$(curl --output /dev/null --insecure --silent --write-out '%{http_code}\n' "https://$(docker exec proxy hostname -I | awk '{print $1}')")
+	if [ "${NANOCLOUD_STATUS}" == "200" ]; then
+		docker-compose -f community/modules/docker-compose-build.yml stop
+		docker-compose -f community/modules/docker-compose-build.yml rm
+		break
+	fi
+	n=$((n+1))
+	sleep 5
+done
