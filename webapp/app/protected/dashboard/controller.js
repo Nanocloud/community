a0e5821be93e253449302ccd74989612d7dda4fb
Eric Cao <eric.cao@nanocloud.com>
2016-05-06 17:03:20 +0200

    load template even when model is not loaded

--- webapp/app/protected/dashboard/controller.js
+++ webapp/app/protected/dashboard/controller.js
@@ -4,20 +4,45 @@ export default Ember.Controller.extend({
 
   session: Ember.inject.service('session'),
 
-  apps: Ember.computed('model.@each', 'model.@each', function() {
-    return this.get('model.apps')
-      .rejectBy('alias', 'hapticDesktop');
-  }),
+  loadState: {
+    application: 0,
+    user: 0,
+    session: 0,
+  },
 
-  users: Ember.computed('model.@each', 'model.@each', function() {
+
+  users: Ember.computed('model.users', 'model.users', function() {
     return this.get('model.users')
       .rejectBy('isAdmin', true);
+  }),
+
+  apps: Ember.computed('model.apps', 'model.apps', function() {
+    return this.get('model.apps')
+      .rejectBy('alias', 'hapticDesktop');
   }),
 
   sessions: Ember.computed('model.@each', 'model.@each', function() {
     return this.get('model.sessions')
       .rejectBy('username', 'Administrator');
   }),
+
+  activator: function() {
+    this.loadData('application', 'apps');
+    this.loadData('user', 'users');
+    this.loadData('session', 'sessions');
+  }.on('init'),
+
+  loadData(data, dest) {
+    this.set('loadState.' + data, 1);
+    this.get('store').query(data, {})
+      .then((response) => {
+        this.set('loadState.' + data, 0);
+        this.set('model.' + dest, response);
+      })
+      .catch((error) => {
+        this.set('loadState.' + data, 2);
+      });
+  },
 
   actions : {
     goToApps() {
--- webapp/app/protected/dashboard/route.js
+++ webapp/app/protected/dashboard/route.js
@@ -2,16 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  loading: function() {
-    var view = this.container.lookup('view:loading').append();
-    this.router.one('didTransition', view, 'destroy');
-  },
-
   model() {
     return Ember.RSVP.hash({
-      apps: this.store.findAll('application'),
-      users: this.store.findAll('user'),
-      sessions: this.store.findAll('session')
+      apps: [],
+      users: [],
+      sessions: [],
     });
   },
 });
