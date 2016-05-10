d2b17593cbeb58f9c5d681b9640655dba982ee0b
Eric Cao <eric.cao@nanocloud.com>
2016-05-10 13:51:11 +0200

    remove commnet on tests

--- webapp/tests/unit/helpers/format-size-test.js
+++ webapp/tests/unit/helpers/format-size-test.js
@@ -3,7 +3,6 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | format size');
 
-// Replace this with your real tests.
 test('it works', function(assert) {
   let result = formatSize([42]);
   assert.ok(result);
