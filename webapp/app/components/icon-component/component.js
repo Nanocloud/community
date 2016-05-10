4b58676afded5e029534ec6ed8e2ef3a36315003
Eric Cao <eric.cao@nanocloud.com>
2016-05-10 12:21:45 +0200

    affect css class in icon-component

--- webapp/app/components/icon-component/component.js
+++ webapp/app/components/icon-component/component.js
@@ -1,4 +1,9 @@
 import TooltipsterComponent from 'ember-cli-tooltipster/components/tool-tipster';
 
 export default TooltipsterComponent.extend({
+  classNames: ['icon-component'],
+  classNameBindings: [
+    'hover-enabled:hover-enabled',
+    'clickable:clickable'
+  ],
 });
