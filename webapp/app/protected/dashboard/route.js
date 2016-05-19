177ef54244e0627c0c4ff3eeb7291a9b5ae42868
Eric Cao <eric.cao@nanocloud.com>
2016-05-19 12:00:22 +0200

    fixing refresh issue in dashboard on route transition

--- webapp/app/protected/dashboard/route.js
+++ webapp/app/protected/dashboard/route.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model() {
+    this.controllerFor('protected.dashboard').activator();
     return Ember.RSVP.hash({
       apps: [],
       users: [],
