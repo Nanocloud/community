66e04e79b689ce0f88bd27293b2ce7069f050a0d
Eric Cao <eric.cao@nanocloud.com>
2016-05-20 10:21:10 +0200

    refresh data on route transit

--- webapp/app/protected/apps/index/route.js
+++ webapp/app/protected/apps/index/route.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  configuration: Ember.inject.service('configuration'),
   model() {
+    this.get('configuration').loadData();
     return this.store.findAll('application', { reload: true });
   }
 });
