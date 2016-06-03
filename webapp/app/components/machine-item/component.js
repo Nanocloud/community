6c361aec02653c064cc22416b07f7dd1974f53fb
Eric Cao <eric.cao@nanocloud.com>
2016-06-03 15:48:21 +0200

    in machine, disable light bulb only when machine is down

--- webapp/app/components/machine-item/component.js
+++ webapp/app/components/machine-item/component.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['machine'],
-  machine: null
+  machine: null,
+
+  shouldEnableLightBulb: Ember.computed('machine.isUp', function() {
+    if (this.get('machine.status') !== 'down') {
+      return true;
+    }
+    return false;
+  })
+
 });
