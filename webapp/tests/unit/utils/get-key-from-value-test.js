f6475d079bf00f5732a3fa29771a60ae4696c30c
Eric Cao <eric.cao@nanocloud.com>
2016-06-14 14:28:24 +0200

    add get-key-from-value util to retrieve a map key by its value

---
+++ webapp/tests/unit/utils/get-key-from-value-test.js
@@ -0,0 +1,10 @@
+import getKeyFromValue from 'nanocloud/utils/get-key-from-value';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get key from value');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getKeyFromValue();
+  assert.ok(result);
+});
