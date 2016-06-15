1d40f808bdfae2f2a4a96d7e7a5a90337ded8df9
William Riancho <william.riancho@nanocloud.com>
2016-06-15 15:24:16 +0200

    use logrus

--- plaza/main_windows.go
+++ plaza/main_windows.go
@@ -36,10 +36,10 @@ const debug = false
 
 func main() {
 	if len(os.Args) < 2 || os.Args[1] != "service" {
-		log.Info("(re)Installing service")
+		logrus.Info("(re)Installing service")
 		err := service.InstallItSelf()
 		if err != nil {
-			log.Error(err)
+			logrus.Error(err)
 		}
 		return
 	}
@@ -55,6 +55,6 @@ func main() {
 
 	err := service.Run()
 	if err != nil {
-		log.Error(err)
+		logrus.Error(err)
 	}
 }
