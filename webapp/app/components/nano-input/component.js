f1a8a247671185de5000d97dacd4d3ff7dea3f86
Eric Cao <eric.cao@nanocloud.com>
2016-06-20 15:56:41 +0200

    refactor nano-input

--- webapp/app/components/nano-input/component.js
+++ webapp/app/components/nano-input/component.js
@@ -4,11 +4,10 @@ export default Ember.Component.extend({
 
   mode: null,
   placeholder: "",
-  errorMessage: "",
   value: "",
   focus: false,
-  isValid: true,
   type: "text",
+  validation: null,
 
   didInsertElement() {
     if (this.get('autofocus') === true) {
@@ -22,29 +21,22 @@ export default Ember.Component.extend({
 
     if (this.get('model')) {
       Ember.defineProperty(this, 'value', Ember.computed.alias(`model.${valuePath}`));
+      Ember.defineProperty(this, 'validation', Ember.computed.oneWay(`model.validations.attrs.${valuePath}`));
     }
-    Ember.defineProperty(this, 'errorMessage', Ember.computed.alias(`model.validations.attrs.${valuePath}.message`));
   },
 
-  fieldIsCorrect: Ember.computed('isValid', 'value', function() {
-    if (this.get('isValid') === true && this.get('value') !== undefined && this.get('value') !== "") {
-      return true;
+  notValidating: Ember.computed.not('validation.isValidating'),
+  hasContent: Ember.computed.notEmpty('value'),
+  isValid: Ember.computed.and('hasContent', 'validation.isValid', 'notValidating'),
+  isInvalid: Ember.computed('validation.isInvalid', function() {
+    if (this.get('value') === "" || this.get('value') === undefined) {
+      return false;
     }
-    return false;
+    return this.get('validation.isInvalid');
   }),
-
-  getErrorMessage: function() {
-
-    if (this.get('model')) {
-      if (this.get('value')) {
-        setTimeout(function() {
-          var errorMessage = this.get('model').get('validations.attrs').get(this.get('valuePath')).get('message');
-          this.set('errorMessage', errorMessage);
-          this.set('isValid', Ember.isEmpty(errorMessage));
-        }.bind(this), 500);
-      }
-    }
-  }.observes('value', 'focus'),
+  showMessage: Ember.computed('validation.isDirty', 'isInvalid', 'didValidate', function() {
+    return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isInvalid');
+  }),
 
   actions: {
     toggleFocus: function() {
